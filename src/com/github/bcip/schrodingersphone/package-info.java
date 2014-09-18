da13283a2af4522b87f39fd0b1bc0a54988ab09c
Wang Qinshi <wangqinshi1995@gmail.com>
2014-09-18 17:03:32 +0800

    new project

---
+++ src/com/github/bcip/schrodingersphone/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Root package of SchrodingersPhone project, by s-quark(Wang Qinshi) and
+ * WJMZBMR(Chen Lijie).
+ */
+/**
+ * @author s-quark
+ *
+ */
+package com.github.bcip.schrodingersphone;

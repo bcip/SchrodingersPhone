646a1ef6a7de4a10d0d45135b34b4a6cd63841f6
Wang Qinshi <wangqinshi1995@gmail.com>
2014-09-20 04:11:47 +0800

    src only

--- src/com/github/bcip/schrodingersphone/server/PreparedThread.java
+++
@@ -1,8 +0,0 @@
-package com.github.bcip.schrodingersphone.server;
-
-public abstract class PreparedThread extends Thread{
-    /**
-     * Return the PreparedStatementSet for the Thread.
-     */
-	public abstract PreparedStatementSet getPreparedStatementSet();
-}

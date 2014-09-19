04512f77ef972e569828461243a20f3539bc0aaf
WJMZBMR <wjmzbmr@gmail.com>
2014-09-19 23:38:11 +0800

    Merge

--- src/com/github/bcip/schrodingersphone/sensor/Uploader.java
+++ src/com/github/bcip/schrodingersphone/sensor/Uploader.java
@@ -6,14 +6,14 @@ import com.github.bcip.schrodingersphone.Record;
 import com.github.bcip.schrodingersphone.SPException;
 
 public class Uploader {
-	public Uploader(InetAddress host, int port){
-		//TODO
+	public Uploader(InetAddress host, int port) {
+		// TODO
 	}
-	
-	public void uploadRecord(Record record) throws SPException{
-		//TODO;
+
+	public void uploadRecord(Record record) throws SPException {
+		// TODO;
 	}
-	
+
 	private InetAddress host;
-	private int port; 
+	private int port;
 }

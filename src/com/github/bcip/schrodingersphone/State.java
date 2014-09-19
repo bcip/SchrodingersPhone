e3cc695cff6ee6668455ebb206ba8722c739149f
Wang Qinshi <wangqinshi1995@gmail.com>
2014-09-20 05:36:32 +0800

    cleaned

--- src/com/github/bcip/schrodingersphone/State.java
+++
@@ -1,6 +0,0 @@
-package com.github.bcip.schrodingersphone;
-public enum State {
-	Walking, Jogging, Upstairs, Downstairs, Sitting, Standing;
-
-	public static final int NUM_STATES = 6;
-}
--- src/com/github/bcip/schrodingersphone/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Root package of SchrodingersPhone project, by s-quark(Wang Qinshi) and
- * WJMZBMR(Chen Lijie).
- */
-/**
- * @author s-quark
- *
- */
-package com.github.bcip.schrodingersphone;

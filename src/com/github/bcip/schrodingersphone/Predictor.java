430fa77788f709fe9fbb419f926bf2a2f7769685
Wang Qinshi <wangqinshi1995@gmail.com>
2014-09-20 20:06:22 +0800

    update Predictor

--- src/com/github/bcip/schrodingersphone/Predictor.java
+++ src/com/github/bcip/schrodingersphone/Predictor.java
@@ -1,8 +1,8 @@
 package com.github.bcip.schrodingersphone;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Predictor {
@@ -22,10 +22,22 @@ public class Predictor {
 		}
 	}
 
+	// <<<<<<< HEAD
+	public void load(InputStream in) throws IOException {
+		Scanner sc = new Scanner(in);
+		for (int i = 0; i < State.NUM_STATES; ++i) {
+			for (int j = 0; j < Feature.FEATURE_LENGTH; j++) {
+				data[i][j] = sc.nextDouble();
+			}
+		}
+	}
+
+	// =======
+	// >>>>>>> 77c5a4be0d9f899289da7e1ae2923b4ba16133f1
 	public State predict(Feature feature) {
 		double[] v = feature.make();
 		int by = -1;
-		double mx = Double.MIN_VALUE;
+		double mx = -Double.MAX_VALUE;
 
 		for (int i = 0; i < State.NUM_STATES; i++) {
 			double dot = 0;
